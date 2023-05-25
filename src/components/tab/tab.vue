f188650319e6a7f5021fd54c49a1b082d2292773
yehaibo <970181822@qq.com>
2023-05-25 16:43:49 +0800

    测试

--- src/components/tab/tab.vue
+++ src/components/tab/tab.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="tab">
     <router-link
-      class="tab-item"
-      v-for="tab in tabs"
-      :key="tab.path"
-      :to="tab.path"
+      class="tab-item" v-for="tab in tabs" :key="tab.path" :to="tab.path"
     >
       <span class="tab-link">
+
         {{tab.name}}
+
       </span>
     </router-link>
   </div>
